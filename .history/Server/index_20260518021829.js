3495a6889a2a922a4a4835af218d00ce73545c4f
mzainnasir010 <221412@students.au.edu.pk>
2026-05-20 00:06:30 +0500

    Change port in index.js port

--- .history/Server/index_20260518021829.js
+++ .history/Server/index_20260518021829.js
@@ -6,7 +6,7 @@ const { startPingService } = require('./services/pingService');
 const predictionRoutes = require('./routes/predictionRoutes');
 
 const app = express();
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 5000;
 
 app.use(cors({
     origin: process.env.FRONTEND_URL,
